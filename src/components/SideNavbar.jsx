6e13e5c76839e12da09d7315a98a619d114c1ec6
Sagar Dutta <duttas874@gmail.com>
2026-03-05 12:59:12 +0530

    Sidenavbar code upgrade

--- src/components/SideNavbar.jsx
+++ src/components/SideNavbar.jsx
@@ -1,31 +1,35 @@
-import { useLocation, useNavigate, Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 import { useSelector } from "react-redux"
 import { Newspaper, Users, MessageCircle, User, LogOut } from "lucide-react"
 import SideNavbarButton from './SideNavbarButton'
 
+const navItems = [
+    { route: "feed", icon: Newspaper },
+    { route: "messages", icon: MessageCircle },
+    { route: "people", icon: Users },
+    { route: "profile", icon: User },
+    { route: "logout", icon: LogOut }
+]
+
 const SideNavbar = () => {
-    const location = useLocation()
     const user = useSelector(store => store.user)
-    const page = location.pathname.split("/")[1]
-    const navigate = useNavigate()
-    const handleClick = (route) => {
-        navigate('/' + route)  // Or navigate(-1) for back, navigate(1) for forward
-    }
 
     return (
         <div className='h-screen w-screen bg-zinc-200'>
             <div className="fixed left-0 z-40 w-20 h-screen py-4 flex flex-col items-center gap-6">
 
-                <div className={`flex items-center justify-center w-14 h-14 rounded-full bg-cover bg-center bg-no-repeat`}
-                    style={{ backgroundImage: `url(${user?.pfp})` }}
-                    onClick={() => { handleClick('profile') }}></div>
+                <NavLink to="/profile">
+                    <div className={`flex items-center justify-center w-14 h-14 rounded-full bg-cover bg-center bg-no-repeat`}
+                        style={{ backgroundImage: `url(${user?.pfp})` }}></div>
+                </NavLink>
 
                 {/* <div className="h-px w-full bg-white"></div> */}
-                <SideNavbarButton icon={Newspaper} isActive={page == "feed"} onClick={() => { handleClick('feed') }} />
-                <SideNavbarButton icon={MessageCircle} isActive={page == "messages"} onClick={() => { handleClick('messages') }} />
-                <SideNavbarButton icon={Users} isActive={page == "people"} onClick={() => { handleClick('people') }} />
-                <SideNavbarButton icon={User} isActive={page == "profile"} onClick={() => { handleClick('profile') }} />
-                <SideNavbarButton icon={LogOut} isActive={page == "logout"} onClick={() => { handleClick('logout') }} />
+
+                {navItems.map(i => <NavLink key={i.route} to={`/${i.route}`}>
+                    {({ isActive }) => (
+                        <SideNavbarButton icon={i.icon} isActive={isActive} />
+                    )}
+                </NavLink>)}
             </div >
 
             <Outlet />
